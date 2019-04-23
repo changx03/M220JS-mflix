2840bd6f83e44848cda3ebbeff19a5a101fd5b46
Luke Chang <luke.x.chang@gmail.com>
2019-04-23 14:34:31 +1200

    completed "Create/Update comments" and "Delete comments"

--- src/dao/commentsDAO.js
+++ src/dao/commentsDAO.js
@@ -45,7 +45,13 @@ export default class CommentsDAO {
     try {
       // TODO Ticket: Create/Update Comments
       // Construct the comment document to be inserted into MongoDB.
-      const commentDoc = { someField: "someValue" }
+      const commentDoc = {
+        name: user.name,
+        email: user.email,
+        movie_id: ObjectId(movieId),
+        text: comment,
+        date: new Date(date),
+      }
 
       return await comments.insertOne(commentDoc)
     } catch (e) {
@@ -69,9 +75,16 @@ export default class CommentsDAO {
       // TODO Ticket: Create/Update Comments
       // Use the commentId and userEmail to select the proper comment, then
       // update the "text" and "date" fields of the selected comment.
+      const updateFields = {}
+      if (text) {
+        updateFields.text = text
+      }
+      if (date) {
+        updateFields.date = new Date(date)
+      }
       const updateResponse = await comments.updateOne(
-        { someField: "someValue" },
-        { $set: { someOtherField: "someOtherValue" } },
+        { _id: ObjectId(commentId), email: userEmail },
+        { $set: updateFields },
       )
 
       return updateResponse
@@ -96,6 +109,7 @@ export default class CommentsDAO {
       // Use the userEmail and commentId to delete the proper comment.
       const deleteResponse = await comments.deleteOne({
         _id: ObjectId(commentId),
+        email: userEmail,
       })
 
       return deleteResponse
